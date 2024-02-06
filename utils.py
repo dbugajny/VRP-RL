65a3eca3f71cb71c62900956da579a8ed99de282
dbugajny <62426794+dbugajny@users.noreply.github.com>
2024-02-06 21:55:55 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -35,6 +35,15 @@ def make_simulation_plot_2(locations, r):
 
 
 def run_environment_simulation(environment, actor, n_steps, approximation_level):
+    """Run simulation - make n_steps, where n_steps is number of needed steps in the worst scenario
+    (n_locations * 2 - vechicle goes to the depot after visiting every node).
+    Each step contains following actions:
+        1. Calculate actor predictions for the best move.
+        2. Take the best action - (approxiamtion is used to make loss functions differentiable)
+        3. Save the best action to the history.
+        4. Update the environment.
+        5. Save the current (new one) vechicle position.
+    """
     approximated_actions = []
     real_actions = []
     for _ in range(n_steps):
@@ -55,6 +64,14 @@ def run_environment_simulation(environment, actor, n_steps, approximation_level)
 
 
 def calculate_full_distance(actions):
+    """Calculate the distance for the given actions according to the Euclidean metric.
+    It is assumed that the vehicle must return to the first node after all actions.
+    Example:
+        - actions: 1 -> 2 -> 3 -> 4
+        - actions_shifted: 4 -> 1 -> 2 -> 3
+        - distance: d(1, 4) + d(2, 1) + d(3, 2) + d(4, 1), d(x, y) = sqrt((x - y) ^ 2)
+    """
+    
     actions_shifted = tf.concat((tf.expand_dims(actions[-1], 0), actions[:-1]), 0)
 
     distances = tf.math.sqrt(tf.reduce_sum(tf.math.square(actions_shifted - actions), -1) + SMALL_NUMBER)
